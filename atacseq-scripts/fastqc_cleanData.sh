09e39d1f78f226fe5f399f8e723473df999688c1
Liu, Zhaoshuo <39985383+XLions@users.noreply.github.com>
2025-11-13 17:12:35 +0000

    Update fastqc_cleanData.sh

--- atacseq-scripts/fastqc_cleanData.sh
+++ atacseq-scripts/fastqc_cleanData.sh
@@ -11,18 +11,18 @@ module load FastQC/0.11.9-Java-11
 
 cd /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc
 
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B1-1-C_R1_001_val_1.fq.gz
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B1-1-C_R2_001_val_2.fq.gz
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B2-1-KD_R1_001_val_1.fq.gz
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B2-1-KD_R2_001_val_2.fq.gz
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B3-1-C-T_R1_001_val_1.fq.gz
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B3-1-C-T_R2_001_val_2.fq.gz
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B4-1-KD-T_R1_001_val_1.fq.gz
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B4-1-KD-T_R2_001_val_2.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B1-1-C_R1_001_val_1.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B1-1-C_R2_001_val_2.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B2-1-KD_R1_001_val_1.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B2-1-KD_R2_001_val_2.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B3-1-C-T_R1_001_val_1.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B3-1-C-T_R2_001_val_2.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B4-1-KD-T_R1_001_val_1.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/B4-1-KD-T_R2_001_val_2.fq.gz
 
 
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/C1-1-C_R1_001_val_1.fq.gz
-# fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/C1-1-C_R2_001_val_2.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/C1-1-C_R1_001_val_1.fq.gz
+fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/C1-1-C_R2_001_val_2.fq.gz
 fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/C2-1-KD_R1_001_val_1.fq.gz
 fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/C2-1-KD_R2_001_val_2.fq.gz
 fastqc -t 12 -o /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/fastqc /rds/projects/g/gendood-preclinomics/EHMT2/ATACSeq_data_Saad/CleanData/C3-1-C-T_R1_001_val_1.fq.gz
